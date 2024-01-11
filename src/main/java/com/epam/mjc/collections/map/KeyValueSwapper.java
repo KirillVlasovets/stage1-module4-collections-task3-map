d0168f9b60e3bdc1c9314899f38037070e03d3c9
Kirill Vlasovets <k.vlasovets@a1qa.com>
2024-01-11 06:35:49 +0400

    solution added

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,16 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> ans = new HashMap<>();
+        for (var key : sourceMap.keySet()) {
+            if ((ans.containsKey(sourceMap.get(key)) && ans.get(sourceMap.get(key)) > key) || !ans.containsKey(sourceMap.get(key))) {
+                ans.put(sourceMap.get(key), key);
+            }
+        }
+        return ans;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,19 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        var sentenceArr = sentence.toLowerCase().split("\\s");
+        Map<String, Integer> ans = new HashMap<>();
+        for (var word : sentenceArr) {
+            if (ans.containsKey(word)) {
+                ans.put(word, ans.get(word) + 1);
+            } else {
+                ans.put(word, 1);
+            }
+        }
+        return ans;
     }
 }
